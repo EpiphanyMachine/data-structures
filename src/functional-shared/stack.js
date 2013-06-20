a93a07ef3c8268b4cff0ff31d99cb0ebdaec4ff6
EpiphanyMachine <epiphanymachine@gmail.com>
2013-06-19 18:00:32 -0700

    finished stack - functional shared

--- src/functional-shared/stack.js
+++ src/functional-shared/stack.js
@@ -1,3 +1,32 @@
 var makeStack = function(){
-  // Hey! Copy your code from src/functional/stack.js and paste it here
+  var newStack = {};
+  newStack.storage = {};
+  newStack.stackSize = 0;
+  _.extend(newStack, stackMethods);
+  return newStack;
+};
+
+var stackMethods = {};
+stackMethods.push = function(value){
+    this.storage[this.stackSize] = value;
+    this.stackSize ++;
+};
+stackMethods.pop = function(){
+  if (this.stackSize > 0) {
+    var temp = this.storage[this.stackSize - 1];
+    delete this.storage[this.stackSize - 1];
+    this.stackSize --;
+    return temp;
+  }
+  return undefined;
+};
+stackMethods.size = function(){
+  return this.stackSize;
+};
+stackMethods.storageSize = function(){
+  var tempSize = 0;
+  for (var i = 0; i < this.storage.length; i++) {
+    tempSize ++;
+  }
+  return tempSize;
 };
