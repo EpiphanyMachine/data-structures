31559ef02c51d4c3085173eaeb615fe73fc501ab
EpiphanyMachine <epiphanymachine@gmail.com>
2013-06-19 18:03:09 -0700

    finished pseudoclassical stack

--- src/pseudoclassical/stack.js
+++ src/pseudoclassical/stack.js
@@ -1,3 +1,28 @@
-var Queue = function() {
-  // Hey! Copy your code from src/prototypal/stack.js and paste it here
+var Stack = function() {
+  this.storage = {};
+  this.stackSize = 0;
+};
+
+Stack.prototype.push = function(value){
+    this.storage[this.stackSize] = value;
+    this.stackSize ++;
+};
+Stack.prototype.pop = function(){
+  if (this.stackSize > 0) {
+    var temp = this.storage[this.stackSize - 1];
+    delete this.storage[this.stackSize - 1];
+    this.stackSize --;
+    return temp;
+  }
+  return undefined;
+};
+Stack.prototype.size = function(){
+  return this.stackSize;
+};
+Stack.prototype.storageSize = function(){
+  var tempSize = 0;
+  for (var i = 0; i < this.storage.length; i++) {
+    tempSize ++;
+  }
+  return tempSize;
 };
