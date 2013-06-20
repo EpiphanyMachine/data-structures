ccd3913edb2418a045a210dc5adac40d09617a8f
EpiphanyMachine <epiphanymachine@gmail.com>
2013-06-19 18:02:42 -0700

    finished pseudoclassical queue

--- src/pseudoclassical/queue.js
+++ src/pseudoclassical/queue.js
@@ -1,3 +1,28 @@
 var Queue = function() {
-  // Hey! Copy your code from src/prototypal/queue.js and paste it here
+  this.storage = {};
+  this.head = 0;
+  this.tail = 0;
+};
+
+Queue.prototype.enqueue = function(value){
+  this.storage[this.tail] = value;
+  this.tail ++;
+};
+Queue.prototype.dequeue = function(){
+  if (this.size() > 0) {
+    this.head ++;
+    var temp = this.storage[this.head - 1];
+    delete this.storage[this.head - 1];
+    return temp;
+  }
+};
+Queue.prototype.size = function(){
+  return this.tail - this.head;
+};
+Queue.prototype.storageSize = function(){
+  var size = 0;
+  for (var key in this.storage){
+    size ++;
+  }
+  return size;
 };
