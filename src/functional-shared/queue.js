f8fb9a33ea0abf1a28c116901bc5ff51669f8332
EpiphanyMachine <epiphanymachine@gmail.com>
2013-06-19 17:59:12 -0700

    Finished Queue - functional shared

--- src/functional-shared/queue.js
+++ src/functional-shared/queue.js
@@ -1,3 +1,32 @@
 var makeQueue = function(){
-	// Hey! Copy your code from src/functional/queue.js and paste it here
+  var newQueue = Object.create(queuePrototype);
+  newQueue.storage = {};
+  newQueue.head = 0;
+  newQueue.tail = 0;
+  return newQueue;
+};
+
+
+var queuePrototype = {};
+queuePrototype.enqueue = function(value){
+  this.storage[this.tail] = value;
+  this.tail ++;
+};
+queuePrototype.dequeue = function(){
+  if (this.size() > 0) {
+    this.head ++;
+    var temp = this.storage[this.head - 1];
+    delete this.storage[this.head - 1];
+    return temp;
+  }
+};
+queuePrototype.size = function(){
+  return this.tail - this.head;
+};
+queuePrototype.storageSize = function(){
+  var size = 0;
+  for (var key in this.storage){
+    size ++;
+  }
+  return size;
 };
