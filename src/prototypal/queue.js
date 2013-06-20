e5d04cf6432c4256834d20c3af0fff7ada5be200
EpiphanyMachine <epiphanymachine@gmail.com>
2013-06-19 18:01:17 -0700

    finished prototypal queue

--- src/prototypal/queue.js
+++ src/prototypal/queue.js
@@ -1,3 +1,30 @@
 var Queue = function() {
-  // Hey! Copy your code from src/functional-shared/queue.js and paste it here
+  var newQueue = Object.create(Queue.prototype);
+  newQueue.storage = {};
+  newQueue.head = 0;
+  newQueue.tail = 0;
+  return newQueue;
+};
+
+Queue.prototype.enqueue = function(value){
+  this.storage[this.tail] = value;
+  this.tail ++;
+};
+Queue.prototype.dequeue = function(){
+  if (this.size() > 0) {
+    this.head ++;
+    var temp = this.storage[this.head - 1];
+    delete this.storage[this.head - 1];
+    return temp;
+  }
+};
+Queue.prototype.size = function(){
+  return this.tail - this.head;
+};
+Queue.prototype.storageSize = function(){
+  var size = 0;
+  for (var key in this.storage){
+    size ++;
+  }
+  return size;
 };
