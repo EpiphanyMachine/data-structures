9407f900bbbd41441a2bbd5d384c32bf45005ff3
EpiphanyMachine <epiphanymachine@gmail.com>
2013-06-19 18:02:14 -0700

    finished prototypal stack

--- src/prototypal/stack.js
+++ src/prototypal/stack.js
@@ -1,3 +1,30 @@
 var Stack = function() {
-  // Hey! Copy your code from src/functional-shared/stack.js and paste it here
+  var newStack = Object.create(Stack.prototype);
+  newStack.storage = {};
+  newStack.stackSize = 0;
+  return newStack;
+};
+
+Stack.prototype.push = function(value){
+    this.storage[this.stackSize] = value;
+    this.stackSize ++;
+};
+Stack.prototype.pop = function(){
+  if (this.stackSize > 0) {
+    var temp = this.storage[this.stackSize - 1];
+    delete this.storage[this.stackSize - 1];
+    this.stackSize --;
+    return temp;
+  }
+  return undefined;
+};
+Stack.prototype.size = function(){
+  return this.stackSize;
+};
+Stack.prototype.storageSize = function(){
+  var tempSize = 0;
+  for (var i = 0; i < this.storage.length; i++) {
+    tempSize ++;
+  }
+  return tempSize;
 };
